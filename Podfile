b589278390e18b22f3b0b2066c1d0ab291539bf6
CamilaMoscatelli <camila@toptierlabs.com>
2016-11-07 13:58:50 -0300

    Pods added to project

---
+++ Podfile
@@ -0,0 +1,19 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Catwang' do
+    pod 'iRate', :git => 'https://github.com/nicklockwood/iRate.git'
+    pod 'DateTools', '~> 1.7.0'
+    pod 'TMCache', '~> 2.1.0'
+    pod 'UIImageTrim', '~> 1.02'
+    pod 'UIImageEffects', '~> 0.0.1'
+    pod 'Haneke', '~> 1.0'
+    pod 'TWRDownloadManager', '~> 1.1.1'
+    pod 'JPSThumbnailAnnotation', '~> 1.0.0'
+    pod 'FCFileManager', '~> 1.0.18'
+    pod 'AFNetworking', '~> 3.0'
+    pod 'Flurry-iOS-SDK/FlurrySDK', '~> 7.8.1'
+    pod 'Flurry-iOS-SDK/FlurryAds', '~> 7.8.1'
+    pod 'iVersion', '~> 1.11.4'
+    pod 'TAOverlay', '~> 2.0.1'
+end
--- stickerApp/external/Social/DataHolder.m
+++ stickerApp/external/Social/DataHolder.m
@@ -11,51 +11,25 @@
 
 @implementation DataHolder
 
-//@synthesize userObject,snapArray,featuredArray,peeksNearArray,myPeeksNames,myPeeksLocations;
-
 static DataHolder *DataHolderSharedInstance = nil;
 
 + (id ) alloc {
-    
     @synchronized([DataHolder class]) {
-        
         NSAssert(DataHolderSharedInstance == nil, @"Attempted to allocate a second instance of a singleton.");
         DataHolderSharedInstance = [super alloc];
         return DataHolderSharedInstance;
-        
     }
     return nil;
 }
 - (id) init {
-    
     self = [super init];
-    if (self != nil) {
-        
-//        NSDictionary *dic=[[NSUserDefaults standardUserDefaults] objectForKey:@"mypeeks"];
-//        myPeeksLocations=[[NSMutableArray alloc] init];
-//        myPeeksNames=[[NSMutableArray alloc] init];
-//        for (NSString *str in [dic objectForKey:@"names"]) {
-//            [myPeeksNames addObject:str];
-//        }
-//        
-//        NSArray *arr=[dic objectForKey:@"locations"];
-//        for (NSDictionary *dic in arr) {
-//            NSNumber *lat=[dic objectForKey:@"lat"];
-//            NSNumber *lng=[dic objectForKey:@"long"];
-//            CLLocationCoordinate2D coord=CLLocationCoordinate2DMake([lat floatValue],[lng floatValue]);
-//            [myPeeksLocations addObject:[NSValue valueWithMKCoordinate:coord]];
-//        }
-    }
     return self;
     
 }
 
 + (DataHolder *) DataHolderSharedInstance {
-    
     @synchronized ([DataHolder class]) {
-        
-        if (!DataHolderSharedInstance)
-        {
+        if (!DataHolderSharedInstance) {
             DataHolderSharedInstance = [[DataHolder alloc] init];
         }
         return DataHolderSharedInstance;
@@ -64,39 +38,24 @@ static DataHolder *DataHolderSharedInstance = nil;
 }
 
 
-
 - (NSInteger) checkUserLikeStatus:(PFObject *)selectedSnap{
-    
     [selectedSnap fetchInBackground];
-    
     NSMutableArray *likeArray= [[NSMutableArray alloc] initWithArray:selectedSnap[@"likes"]];
-    
     for (NSString *userLike in likeArray) {
-        
         if ([userLike isEqualToString:[DataHolder DataHolderSharedInstance].userObject.objectId]){
-            
             return 1;
-            
-        } 
-        
-    }
-    
-    NSMutableArray *dislikesarray= [[NSMutableArray alloc] initWithArray:selectedSnap[@"dislikes"]];
-    
-    for (NSString *userLike in dislikesarray) {
-        
-        if ([userLike isEqualToString:[DataHolder DataHolderSharedInstance].userObject.objectId]){
-            
-            return 2;
-            
         }
     }
-    
-    
+    NSMutableArray *dislikesarray= [[NSMutableArray alloc] initWithArray:selectedSnap[@"dislikes"]];
+    for (NSString *userLike in dislikesarray) {
+        if ([userLike isEqualToString:[DataHolder DataHolderSharedInstance].userObject.objectId]){
+            return 2;
+        }
+    }
+
     return 0;
     
 }
-
 
 
 @end
